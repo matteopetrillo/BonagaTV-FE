95a22dcd32fd1d09a205144736bdaea09572cc2b
Matteo Petrillo <commercial@matteopetrillo.info>
2024-04-09 16:28:29 +0200

    first commit

---
+++ src/components/ChannelCard.vue
@@ -0,0 +1,101 @@
+<template>
+    <v-card class="mainCard ma-3" border>
+        <iframe 
+        class="player"
+        id="ce1e9100-ab79-714b-0ef6-d31d21f25b8f" 
+        src="https://iframe.dacast.com/live/e2eec26b-7144-f2c7-b0df-0153dcd0c24b/7867b42d-6fe3-6a2f-ab48-318ab68805fc" 
+        frameborder="0"
+        allowfullscreen 
+        webkitallowfullscreen 
+        mozallowfullscreen 
+        oallowfullscreen 
+        msallowfullscreen>
+        </iframe>
+        <div class="channel">
+            <v-avatar class="channelIcon">
+                <svg-icon type="mdi" :path="logo" size="60"></svg-icon>
+            </v-avatar>
+            <div class="channelInfo">
+                <div class="channelName">{{ name }}</div>
+                <div class="liveDescription">2° Tappa Campionato 2024</div>
+            </div>
+            <div class="liveIcon">
+                <svg-icon :color="color" type="mdi" :path="circle"></svg-icon>
+            </div>
+        </div>
+    </v-card>
+</template>
+
+<script> 
+import SvgIcon from '@jamescoyle/vue-icon'
+import { mdiAccountCircle   } from '@mdi/js'
+import { mdiCircle } from '@mdi/js'
+
+export default {
+    name: "ChannelCard",
+    props: {
+        name: {
+            type: String
+        },
+        color: {
+            type: String
+        }
+        
+    },
+    components: {
+		SvgIcon
+    },
+    data() {
+        return {
+            logo: mdiAccountCircle,
+            circle: mdiCircle
+        }
+    }
+}
+
+</script>
+
+<style scoped>
+
+.mainCard {
+    max-width: 18.5rem;
+    max-height: 13rem;
+    margin: auto;
+    box-shadow: 4px 4px 4px rgba(0,0,0,0.1);
+}
+
+
+.channel {
+    display: flex;
+    align-items: center;
+    padding: 2px;
+}
+
+.channelInfo {
+    flex: 1;
+}
+
+.channelName {
+    font-size: 1rem;
+    font-weight: bold;
+}
+
+.liveDescription {
+    font-size: 0.8rem;
+    color: grey;
+    margin-bottom: 0.4rem;
+
+}
+
+.channelIcon {
+    margin-left: 0.7rem;
+    margin-right: 0.7rem;
+    margin-bottom: 8px;
+}
+
+.liveIcon {
+    justify-content: right;
+    margin-right: 0.7rem;
+}
+
+</style>
---
+++ src/components/SponsorFooter.vue
@@ -0,0 +1,27 @@
+<template>
+    <v-footer app color="grey-darken-2" class="footer">
+        <v-flex xs1>
+            <v-row justify="center" >
+                <p class="text-h5 text-white">
+                    Grazie ai nostri sponsor!
+                </p> 
+            </v-row>
+        </v-flex>              
+    </v-footer>
+</template>
+
+<script>
+    export default {
+        name: "SponsorFooter"
+    }
+</script>
+
+<style scoped>
+.footer {
+    height: 20%;
+    width: 100%;
+    padding: 25px;
+    flex-direction: column;
+}
+
+</style>
