742da952f6a4341b090ea7091b04c43499ad8b32
Matteo Petrillo <commercial@matteopetrillo.info>
2024-04-28 11:58:03 +0200

    update loading

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,10 @@
   <template>
-    <v-main class="bg-white-2">
-      <SpecialEvent 
-      v-if="this.specialEvent != null" 
-      :nomeEvento="this.specialEvent.nomeEvento"
-      :logoSrc="this.specialEvent.logoEventoSrc"
-      :idEvento="this.specialEvent.idEvento"
-      ></SpecialEvent>
+    <div v-if="!this.readyToShow">
+      <v-progress-circular color="orange" indeterminate></v-progress-circular>
+    </div>
+    <v-main v-else class="bg-white-2">
+      <SpecialEvent v-if="this.specialEvent != null" :nomeEvento="this.specialEvent.nomeEvento"
+        :logoSrc="this.specialEvent.logoEventoSrc" :idEvento="this.specialEvent.idEvento"></SpecialEvent>
       <v-container>
         <v-row>
           <v-col class="mb-n6" cols="12">
@@ -14,19 +13,13 @@
           <v-item-group>
             <v-container>
               <v-row>
-                <v-col v-for="(canale, index) in this.canaliOnline" 
-                :key="index" 
-                cols="12" xs="12" sm="10" md="4" :lg="getNumCols(this.canaliOnline)"
-                :class="{'pr-10' : this.canaliOnline.length < 3}">
+                <v-col v-for="(canale, index) in this.canaliOnline" :key="index" cols="12" xs="12" sm="10" md="4"
+                  :lg="getNumCols(this.canaliOnline)" :class="{'pr-10' : this.canaliOnline.length < 3}">
                   <v-item>
-                    <ChannelCard
-                    :idCanale="canale.id"
-                    :nomeCanale="canale.nomeCanale" 
-                    :nomeEvento="canale.nomeEvento" 
-                    :srcLink="canale.streamingSrc" 
-                    :isLive="true"/>
+                    <ChannelCard :idCanale="canale.id" :nomeCanale="canale.nomeCanale" :nomeEvento="canale.nomeEvento"
+                      :srcLink="canale.streamingSrc" :isLive="true" />
                   </v-item>
-                </v-col>             
+                </v-col>
               </v-row>
             </v-container>
           </v-item-group>
@@ -37,16 +30,11 @@
           <v-item-group>
             <v-container>
               <v-row>
-                <v-col v-for="(canale, index) in this.canaliOffline" 
-                :key="index" 
-                cols="12" xs="12" sm="10" md="4" :lg="getNumCols(this.canaliOffline)">
+                <v-col v-for="(canale, index) in this.canaliOffline" :key="index" cols="12" xs="12" sm="10" md="4"
+                  :lg="getNumCols(this.canaliOffline)">
                   <v-item>
-                    <ChannelCard 
-                    :idCanale="canale.id"
-                    :nomeCanale="canale.nomeCanale" 
-                    :nomeEvento="canale.nomeEvento" 
-                    srcLink="" 
-                    :isLive="false"/>
+                    <ChannelCard :idCanale="canale.id" :nomeCanale="canale.nomeCanale" :nomeEvento="canale.nomeEvento"
+                      srcLink="" :isLive="false" />
                   </v-item>
                 </v-col>
               </v-row>
@@ -54,9 +42,9 @@
           </v-item-group>
         </v-row>
       </v-container>
-      
+
     </v-main>
-</template>
+  </template>
 
 <script>
   import { mapActions, mapGetters } from 'vuex'
@@ -79,12 +67,14 @@
     },
     created() {
       this.getData();
+      this.readyToShow = true;
     },
     data() {
       return {
         specialEvent: null,
         canaliOnline: null,
-        canaliOffline: null
+        canaliOffline: null,
+        readyToShow: false
       }
     },
     methods: {
