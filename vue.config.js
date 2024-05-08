8e7a0026728b3407aab1edae7e6f558a1924a917
Matteo Petrillo <commercial@matteopetrillo.info>
2024-05-08 16:29:53 +0200

    fix bug

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,7 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: `/it`,
   pluginOptions: {
     i18n: {
       locale: 'it',
